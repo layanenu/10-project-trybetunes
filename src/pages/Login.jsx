9d42431fbfa98e1fe5923c5ca842b9893c106265
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-08-30 21:00:07 -0300

    Requisito 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,74 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from '../components/Carregando';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      carregando: false,
+      saveButtonDisabled: true,
+      redirect: false,
+    };
+  }
+
+  nameInputChange = ({ target }) => {
+    const { name, value } = target;
+    const minimumCharacters = 3;
+    this.setState({
+      [name]: value,
+    });
+
+    if (value.length >= minimumCharacters) {
+      this.setState({
+        saveButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        saveButtonDisabled: true,
+      });
+    }
+  };
+
+  ButtonNameClick = async () => {
+    const { name } = this.state;
+    this.setState({
+      carregando: true, // conforme espera a resposta da api o estado carregando é true
+    });
+    await createUser({ name });
+    this.setState({
+      carregando: false,
+      redirect: true,
+    });
+  };
+
   render() {
+    const { name, carregando, saveButtonDisabled, redirect } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        { carregando && <Carregando /> }
+        <form>
+          <input
+            type="text"
+            data-testid="login-name-input"
+            placeholder="Nome"
+            name="name"
+            value={ name }
+            onChange={ this.nameInputChange }
+          />
+        </form>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ saveButtonDisabled }
+          onClick={ this.ButtonNameClick }
+        >
+          Entrar
+        </button>
+        { redirect && <Redirect to="/search" />}
+      </div>
     );
   }
 }
